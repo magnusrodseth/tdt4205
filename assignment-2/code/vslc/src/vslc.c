ca2d030a54ed0935734da955f93a8f2ab894f437
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2023-02-03 17:50:32 +0100

    wip

--- assignment-2/code/vslc/src/vslc.c
+++ assignment-2/code/vslc/src/vslc.c
@@ -4,42 +4,41 @@
 #include <vslc.h>
 
 /* Command line option parsing for the main function */
-static void options ( int argc, char **argv );
+static void options(int argc, char **argv);
+
 static bool
-    print_full_tree = false;
+        print_full_tree = false;
 
 /* Entry point */
-int main ( int argc, char **argv )
-{
-    options ( argc, argv );
+int main(int argc, char **argv) {
+    options(argc, argv);
 
     yyparse();       // Generated from grammar/bison, constructs syntax tree
     yylex_destroy(); // Free buffers used by flex
 
     // Operations in tree.c
-    if ( print_full_tree )
-        print_syntax_tree ();
+    if (print_full_tree)
+        print_syntax_tree();
 
-    destroy_syntax_tree ();     // In tree.c
+    destroy_syntax_tree();     // In tree.c
 }
 
 static const char *usage =
-"Command line options\n"
-"\t-h\tOutput this text and halt\n"
-"\t-t\tOutput the full syntax tree\n";
+        "Command line options\n"
+        "\t-h\tOutput this text and halt\n"
+        "\t-t\tOutput the full syntax tree\n";
 
-static void options ( int argc, char **argv )
-{
+static void options(int argc, char **argv) {
     int o;
-    while ( (o=getopt(argc,argv,"ht")) != -1 )
-    {
-        switch ( o )
-        {
+    while ((o = getopt(argc, argv, "ht")) != -1) {
+        switch (o) {
             case 'h':
-                printf ( "%s:\n%s", argv[0], usage );
-                exit ( EXIT_SUCCESS );
+                printf("%s:\n%s", argv[0], usage);
+                exit(EXIT_SUCCESS);
                 break;
-            case 't':   print_full_tree = true;             break;
+            case 't':
+                print_full_tree = true;
+                break;
         }
     }
 }
